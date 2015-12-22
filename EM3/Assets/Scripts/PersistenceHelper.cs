e2eef8b9835d7fb2fda87a525e77cd14381fbf2b
Paco <fr4p4ve@gmail.com>
2015-12-21 19:22:05 -0500

    Avance Buscador.
    Inicio de la codificación del buscador.

--- EM3/Assets/Scripts/PersistenceHelper.cs
+++ EM3/Assets/Scripts/PersistenceHelper.cs
@@ -399,3 +399,5 @@ using UnityEngine;
 
 
 
+
+
---
+++ EM3/Assets/Scripts/SearchScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Assets;
+
+public class SearchScript : MonoBehaviour {
+
+    private PersistenceHelper ph;
+    private List<ModeloDescripcion> diccionario;
+	// Use this for initialization
+	void Start () {
+
+        ph = new PersistenceHelper();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private List<ModeloDescripcion> CargarDiccionario()
+    {
+        diccionario = ph.CargarDescripciones();//se puede cargar el xml que sirva de diccionario
+
+        Debug.Log("Number of descriptions on XML file: " + diccionario.Count);
+
+        return diccionario;
+    }
+
+    //private void 
+}
