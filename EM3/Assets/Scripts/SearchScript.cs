4387aad3fb02ec0d284812607fef28396e76ae07
Paco <fr4p4ve@gmail.com>
2015-12-23 10:29:34 -0500

    Avance Buscador
    Se realizo un traductor de palabras del español a nombres de modelos en
    latín. Se avanzo el algoritmos de búsqueda por palabras.

--- EM3/Assets/Scripts/SearchScript.cs
+++ EM3/Assets/Scripts/SearchScript.cs
@@ -3,15 +3,22 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using Assets;
+using System;
 
 public class SearchScript : MonoBehaviour {
 
-    private PersistenceHelper ph;
-    private List<ModeloDescripcion> diccionario;
+    private PersistenceHelper PersistanceHelper;
+    private List<ModeloDescripcion> Diccionario;
+    private List<String> PalabrasTraducidas;
+    private List<String> PracticasResultado;
 	// Use this for initialization
 	void Start () {
 
-        ph = new PersistenceHelper();
+        PersistanceHelper = new PersistenceHelper();
+        Diccionario = new List<ModeloDescripcion>();
+        PalabrasTraducidas = new List<String>();
+        PracticasResultado = new List<String>();
+        DebugObtenerTraduccion("Corazon");
 
 	}
 	
@@ -22,12 +29,48 @@ public class SearchScript : MonoBehaviour {
 
     private List<ModeloDescripcion> CargarDiccionario()
     {
-        diccionario = ph.CargarDescripciones();//se puede cargar el xml que sirva de diccionario
+        List<ModeloDescripcion> DiccionarioTemp;
 
-        Debug.Log("Number of descriptions on XML file: " + diccionario.Count);
+        DiccionarioTemp = PersistanceHelper.CargarDescripciones();//se puede cargar el xml que sirva de diccionario
 
-        return diccionario;
+        Debug.Log("Number of descriptions on XML file: " + DiccionarioTemp.Count);
+
+        return DiccionarioTemp;
     }
+
+    private List<String> ObtenerTraduccion(string PalabraBusqueda)
+    {
+        List<String> PalabrasTraducidasTemp = new List<string>();
+
+        Diccionario = CargarDiccionario();
+
+        foreach(ModeloDescripcion desc in Diccionario)
+        {
+            if (desc.Descripcion.Contains(PalabraBusqueda))
+            {
+                PalabrasTraducidasTemp.Add(desc.Nombre);
+            }
+        }
+
+        return PalabrasTraducidasTemp;
+    }
+
+    private void DebugObtenerTraduccion(string PalabraBusqueda)
+    {
+        PalabrasTraducidas = ObtenerTraduccion(PalabraBusqueda);
+
+        foreach(String palabra in PalabrasTraducidas)
+        {
+            Debug.Log(palabra);
+        }
+    }
+
+    //private List<String> ObtenerRessultadosBusqueda(string PalabraBusqueda)
+    //{
+    //    PalabrasTraducidas = ObtenerTraduccion(PalabraBusqueda);
+
+
+    //}
 
     //private void 
 }
